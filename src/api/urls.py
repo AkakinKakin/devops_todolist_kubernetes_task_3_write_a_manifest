6ba9a57db958b215457804dfba99b261b990476a
Eldar <eldardemchenko19@mail.com>
2025-08-22 15:58:58 +0300

    Solution

--- src/api/urls.py
+++ src/api/urls.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.urls import include, path
 from rest_framework.routers import DefaultRouter
 
@@ -10,5 +11,7 @@ router.register(r"todos", views.TodoViewSet)
 
 app_name = "api"
 urlpatterns = [
-    path("", include(router.urls))
+    path("", include(router.urls)),
+    path("/health", views.health_check, name="health"),
+    path("/ready", views.readiness_check, name="ready"),
 ]
